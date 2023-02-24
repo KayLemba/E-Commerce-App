a2ef3004397064ef82177e7740f5cbeb026876f8
Kalolo Lemba <kalololemba@Kalolos-MacBook-Air.local>
2023-02-24 17:23:41 +0200

    Update product.js file

--- src/Product.js
+++ src/Product.js
@@ -7,7 +7,6 @@ function Product({ id, title, image, price, rating }) {
   const [{ basket }, dispatch] = useStateValue();
 
   const addToBasket = () => {
-    // dispatch the item into the data layer
     dispatch({
       type: "ADD_TO_BASKET",
       item: {
